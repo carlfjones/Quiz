1ca57ad55fa50bc6527f52c30166f8e69dc2e912
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-03-02 09:25:07 +0000

    Add question and answer display from .json file

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,42 @@ function getQuestions () {
     request.onreadystatechange = function () {
         if (this.readyState = 4 && this.status == 200) {
             questions = JSON.parse(this.responseText).questions;
-            console.log(questions);
+
+            questionsCount = questions.length;
+            
+            currentQuestion = 0;
             
         }
     }
+    request.open("GET", "/questions.json",  false);
+    request.send();
 }
+
+function displayQuestion (question) {
+    question_title_elem.innerHTML = "";
+    answers_elem.innerHTML = "";
+
+    let question_title = document.createTextNode(question.title);
+    question_title_elem.appendChild(question_title);
+
+    question.answers.forEach(answer => {
+        let label = document.createElement("label");
+
+        let answer_input = document.createElement("input");
+        answer_input.setAttribute("type", "radio");
+        answer_input.setAttribute("name", "answer");
+        answer_input.setAttribute("value", "answer.id");
+        answer_input.classList.add("answer");
+
+        let answer_title = document.createTextNode(answer.answer);
+        label.appendChild(answer_input);
+        label.appendChild(answer_title);
+
+        answers_elem.appendChild(label);
+        
+    });
+}
+
+// Initialisation
+getQuestions();
+displayQuestion(questions[currentQuestion]);
